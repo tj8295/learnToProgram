c4ff5c4fb4f72fe108a836d1c4c61702075fda37
TJH <tch399@gmail.com>
2013-12-08 18:07:01 -0500

    Corrected overwritten file

--- 2_3_authors_age_in_years.rb
+++ 2_3_authors_age_in_years.rb
@@ -1,11 +1,6 @@
 #Hours in a year.​ How many hours are in a year?
 days_in_a_year = 365
 hours_in_a_day = 24
-minutes_in_a_hour = 60
-seconds_in_a_minute = 60
+hours_in_a_year = days_in_a_year * hours_in_a_day
 
-seconds_in_a_year = seconds_in_a_minute * minutes_in_a_hour * hours_in_a_day * days_in_a_year
-puts "Seconds in a year " + seconds_in_a_year.to_s
-authors_age = 1160000000
-authors_age_in_years = authors_age / seconds_in_a_year
-puts "Author is: " + authors_age_in_years.to_s + " old."
+puts "Hours in a year: " + hours_in_a_year.to_s
